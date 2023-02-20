bc9982e17e3bd18bc34e6fc5ad06b9d91d7fa684
rishipalb <rishipal.b@gmail.com>
2023-02-20 14:51:26 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 import pandas as pd
 
-import ydata_profiling
+from ydata_profiling import ProfileReport
 
 import streamlit as st
 
 from streamlit_pandas_profiling import st_profile_report
 
-from pandas_profiling import ProfileReport
+#from pandas_profiling import ProfileReport
 
 
 
